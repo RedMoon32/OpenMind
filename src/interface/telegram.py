c90a48b8dbf39cf045af4b55f3d40cb6a0dc93b9
Denis <den1-1@mail.ru>
2018-06-11 18:24:33 +0300

    Extracted module IntentDetector from Assistant

--- src/interface/telegram.py
+++ src/interface/telegram.py
@@ -3,6 +3,7 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 
 from assistant.answer import AssistantAnswer
+from assistant.intent_detector import IntentDetector
 from configs.config_constants import StartMessageKey, TokenKey, PrintMessages
 from assistant.assistant import Assistant
 from interface.base_interface import BaseInterface
@@ -14,12 +15,11 @@ STOP_MESSAGE_KEY = "stop_message_key"
 
 class Telegram(BaseInterface):
 
-    def __init__(self, language_model, app_dict, w2v, message_bundle, config):
+    def __init__(self, language_model, intent_detector: IntentDetector, message_bundle, config):
         super().__init__(message_bundle, config)
 
         self.__language_model = language_model
-        self.__app_dict = app_dict
-        self.__w2v = w2v
+        self.__intent_detector = intent_detector
         self.__token = self.config[TokenKey]
         self.__START_MESSAGE_KEY = self.config[StartMessageKey]
         self.__user_assistant_dict: Dict[int, Assistant] = {}
@@ -40,8 +40,8 @@ class Telegram(BaseInterface):
             print((USER_ASKS_PATTERN.format(user_id, user_name, request)))
         assistant: Assistant = self.__user_assistant_dict.get(user_id, None)
         if assistant is None:
-            assistant: Assistant = Assistant(self.__language_model, self.message_bundle, self.__app_dict,
-                                             self.config, w2v=self.__w2v, user_id=user_id)
+            assistant: Assistant = Assistant(self.__language_model, self.message_bundle,
+                                             self.config, self.__intent_detector, user_id=user_id)
             self.__user_assistant_dict[user_id] = assistant
         answer = assistant.process_request(request)
         message = answer.message

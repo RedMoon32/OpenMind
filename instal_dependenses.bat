84d683fe81e47a9cb1e0eb21f21ef743616f2fcc
Denis <den1-1@mail.ru>
2018-06-11 22:29:42 +0300

    Rename file

--- instal_dependenses.bat
+++
@@ -1,7 +0,0 @@
-pip install python-telegram-bot --upgrade
-pip install wolframalpha
-pip install pyemd
-pip install pycorenlp
-pip install yandex.translate
-pip install -U gensim
-pip install wolframalpha
